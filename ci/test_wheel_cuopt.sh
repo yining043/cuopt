7a69f21f9298aa207c37fb96ccfc2b9fd08852f6
Trevor McKay <tmgithub1@gmail.com>
2025-07-30 18:33:52 -0400

    add a simple smoketest for cvxpy integration (#199)
    This is a simple smoketest to look for breakage and any abnormal behavior with respect to runtime from cuopt Python API changes. It does not do any exhaustive performance testing or building.
    
    depends on #235
    
    Authors:
      - Trevor McKay (https://github.com/tmckayus)
      - James Lamb (https://github.com/jameslamb)
      - Ramakrishnap (https://github.com/rgsl888prabhu)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/NVIDIA/cuopt/pull/199

--- ci/test_wheel_cuopt.sh
+++ ci/test_wheel_cuopt.sh
@@ -17,19 +17,33 @@
 
 set -euo pipefail
 
+# sets up a constraints file for 'pip' and puts its location in an exported variable PIP_EXPORT,
+# so those constraints will affect all future 'pip install' calls
 source rapids-init-pip
 
 # Download the packages built in the previous step
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 CUOPT_MPS_PARSER_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_mps_parser" rapids-download-wheels-from-github python)
+CUOPT_SH_CLIENT_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_sh_client" rapids-download-wheels-from-github python)
 CUOPT_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
 LIBCUOPT_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcuopt_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
+
+# update pip constraints.txt to ensure all future 'pip install' (including those in ci/thirdparty-testing)
+# use these wheels for cuopt packages
+cat > "${PIP_CONSTRAINT}" <<EOF
+cuopt-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo ${CUOPT_WHEELHOUSE}/cuopt_${RAPIDS_PY_CUDA_SUFFIX}-*.whl)
+cuopt-mps-parser @ file://$(echo ${CUOPT_MPS_PARSER_WHEELHOUSE}/cuopt_mps_parser-*.whl)
+cuopt-sh-client @ file://$(echo ${CUOPT_SH_CLIENT_WHEELHOUSE}/cuopt_sh_client-*.whl)
+libcuopt-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo ${LIBCUOPT_WHEELHOUSE}/libcuopt_${RAPIDS_PY_CUDA_SUFFIX}-*.whl)
+EOF
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 rapids-pip-retry install \
     --extra-index-url=https://pypi.nvidia.com \
+    --constraint "${PIP_CONSTRAINT}" \
     "${CUOPT_MPS_PARSER_WHEELHOUSE}"/cuopt_mps_parser*.whl \
     "$(echo "${CUOPT_WHEELHOUSE}"/cuopt*.whl)[test]" \
+    "${CUOPT_SH_CLIENT_WHEELHOUSE}"/cuopt_sh_client*.whl \
     "${LIBCUOPT_WHEELHOUSE}"/libcuopt*.whl
 
 python -c "import cuopt"
@@ -59,3 +73,5 @@ timeout 10m bash ./python/libcuopt/libcuopt/tests/test_cli.sh
 # Run Python tests
 RAPIDS_DATASET_ROOT_DIR=./datasets timeout 30m python -m pytest --verbose --capture=no ./python/cuopt/cuopt/tests/
 
+# run cvxpy integration tests
+./ci/thirdparty-testing/run_cvxpy_tests.sh
---
+++ ci/thirdparty-testing/run_cvxpy_tests.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e -u -o pipefail
+
+echo "building 'cvxpy' from source"
+git clone https://github.com/cvxpy/cvxpy.git
+pushd ./cvxpy || exit 1
+pip wheel \
+    -w dist \
+    .
+
+# NOTE: installing cvxpy[CUOPT] alongside CI artifacts is helpful to catch dependency conflicts
+echo "installing 'cvxpy' with cuopt"
+python -m pip install \
+    --constraint "${PIP_CONSTRAINT}" \
+    --extra-index-url=https://pypi.nvidia.com \
+    --extra-index-url=https://pypi.anaconda.org/rapidsai-wheels-nightly/simple \
+    'pytest-error-for-skips>=2.0.2' \
+    "$(echo ./dist/cvxpy*.whl)[CUOPT,testing]"
+
+# ensure that environment is still consistent (i.e. cvxpy requirements do not conflict with cuopt's)
+pip check
+
+echo "running 'cvxpy' tests"
+timeout 3m python -m pytest \
+    --verbose \
+    --capture=no \
+    --error-for-skips \
+    -k "TestCUOPT" \
+    ./cvxpy/tests/test_conic_solvers.py
