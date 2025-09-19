2aa9e89f443e7c44c957138e711010b6c837f836
Akif ÇÖRDÜK <akifcorduk@gmail.com>
2025-09-19 16:00:44 +0200

    Fix bug in fixed_problem_computation (#403)
    The issue was the problem check that some constraint values were going -inf and nan. This was caught by exceptions but it didn't crash the process as it is caught. We need more graceful handling of exceptions: #264
    
    Authors:
      - Akif ÇÖRDÜK (https://github.com/akifcorduk)
    
    Approvers:
      - Alice Boucher (https://github.com/aliceb-nv)
      - Ramakrishnap (https://github.com/rgsl888prabhu)
    
    URL: https://github.com/NVIDIA/cuopt/pull/403

--- cpp/src/mip/problem/problem.cu
+++ cpp/src/mip/problem/problem.cu
@@ -1278,6 +1278,7 @@ void problem_t<i_t, f_t>::compute_integer_fixed_problem()
     return;
   }
   rmm::device_uvector<f_t> assignment(n_variables, handle_ptr->get_stream());
+  thrust::fill(handle_ptr->get_thrust_policy(), assignment.begin(), assignment.end(), 0.);
   integer_fixed_problem = std::make_shared<problem_t<i_t, f_t>>(get_problem_after_fixing_vars(
     assignment, integer_indices, integer_fixed_variable_map, handle_ptr));
   integer_fixed_problem->check_problem_representation(true);
