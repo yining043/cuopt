e1bf540db680bcd6fa48a2b0374cb1115b1080b7
James Lamb <jlamb@nvidia.com>
2025-09-23 10:54:34 -0500

    build pure-Python wheels without waiting for dependencies (#409)
    Looking closely at the CI dependency graphs while working on #122, I noticed some opportunities to improve the end-to-end time for PR CI and nightly builds.
    
    `wheel-build-{libB}` jobs only need to wait for `wheel-build-{libA}` jobs to complete if **building** `libB` wheels requires `libA`. This updates job dependencies here to follow that rule.
    
    * `wheel-build-cuopt-server`: start building immediately
    * `wheel-build-cuopt-sh-client`: start building immediately
    * `build-images`: adds `wheel-build-cupot-sh-client` to jobs this needs to wait for (branch / nightly builds only)
      - https://github.com/NVIDIA/cuopt/blob/933d810e517567f4b5cb3cd507b350eb30457a70/ci/docker/Dockerfile#L65
    
    This PR also remove some unnecessary builds. This project is currently running 16 `wheel-build-cuopt-server` jobs (2 CUDA major x 2 CPU architectures x 4 Python versions) when it only needs 2 (1 per CUDA major version), because it publishes `py3-none-any` wheels (https://pypi.anaconda.org/rapidsai-wheels-nightly/simple/cuopt-server-cu13).
    
    * removes duplicate `cuopt-server` builds
    * fixes artifact downloads of `cuopt-sh-client` in nightly tests
    
    ## Issue
    
    Noticed while working towards #122
    
    ## Notes for Reviewers
    
    ### Benefits of these changes
    
    Reduced end-to-end time for PR builds and probably for branch/nightly builds.
    
    Reduced risk of failed or incorrect container-image builds resulting from `build-images` starting before there are `cuopt-sh-client` packages available.
    
    14 fewer `wheel-build-cuopt-server` jobs per PR 😁 
    
    #
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Ramakrishnap (https://github.com/rgsl888prabhu)
    
    URL: https://github.com/NVIDIA/cuopt/pull/409

--- ci/test_self_hosted_service.sh
+++ ci/test_self_hosted_service.sh
@@ -22,7 +22,7 @@ source rapids-init-pip
 # Download the cuopt built in the previous step
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
 CUOPT_MPS_PARSER_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_mps_parser" rapids-download-wheels-from-github python)
-CUOPT_SERVER_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_server_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
+CUOPT_SERVER_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_server_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-github python)
 CUOPT_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="cuopt_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
 LIBCUOPT_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcuopt_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 
